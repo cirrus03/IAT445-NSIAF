5c41a51b159cef73e23d0d2b6321670ee6e8801b
cirrus03 <selenachang.y@gmail.com>
2026-02-06 22:39:45 -0800

    connected player health and UI
    - made a player prefab
    - made a "hazard" prefab just for testing between scenes

--- Assets/UI/HealthBarScript.cs
+++ Assets/UI/HealthBarScript.cs
@@ -6,22 +6,25 @@ public class HealthBarScript : MonoBehaviour
 {
     public Slider healthBarSlider;
     public TextMeshProUGUI healthBarValueText;
+    [SerializeField] private Health playerHealth;
 
-    public int maxHealth;
-    public int currHealth;
+    private int maxHealth;
+    // public int currHealth;
 
     void Start()
     {
-        currHealth = maxHealth; //set current health to max
+        // currHealth = maxHealth; //set current health to max
+        maxHealth = (int) playerHealth.currentHealth;
     }
 
     void Update()
     {
         //set health bar text
-        healthBarValueText.text = currHealth.ToString() + "/" + maxHealth.ToString();
+        // healthBarValueText.text = currHealth.ToString() + "/" + maxHealth.ToString();
+        healthBarValueText.text = playerHealth.currentHealth.ToString() + "/" + maxHealth.ToString();
         
         //set slider values
-        healthBarSlider.value = currHealth;
+        healthBarSlider.value = (int)playerHealth.currentHealth;
         healthBarSlider.maxValue = maxHealth;
     }
 }
