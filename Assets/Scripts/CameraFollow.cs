e93d2f258ee33cd7989f1680521e0c979c2061ce
renoutoften <nguyenren64@gmail.com>
2026-02-09 02:58:11 -0800

    made the camera stiff! for now LOL

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,19 +5,11 @@ using System.Collections.Generic;
 public class CameraFollow : MonoBehaviour
 {
     private Vector3 offset = new Vector3(0f, 0f, -10f);
-    private float smoothTime = 0.25f;
-    private Vector3 velocity = Vector3.zero;
     [SerializeField] private Transform target;
     void LateUpdate()
     {
         if (target == null) return;
 
-        Vector3 targetPosition = target.position + offset;
-        transform.position = Vector3.SmoothDamp(
-            transform.position,
-            targetPosition,
-            ref velocity,
-            smoothTime
-        );
+        transform.position = target.position + offset;
     }
 }
