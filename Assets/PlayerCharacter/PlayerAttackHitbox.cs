0b84fe595aab02bad5b1cccfb08ba6a31f428e11
notmarkyang <notmarkyang@gmail.com>
2026-02-10 00:03:56 -0800

    Moved player to LabAreaClone
    +Player to LabAreaClone
    +Enemies
    +Dash.

--- Assets/PlayerCharacter/PlayerAttackHitbox.cs
+++ Assets/PlayerCharacter/PlayerAttackHitbox.cs
@@ -5,18 +5,33 @@ public class PlayerAttackHitbox : MonoBehaviour
     public float damage = 1f;
 
     [Header("Enemy Knockback")]
-    public float knockbackX = 6f;
-    public float knockbackY = 2f;
+    public float knockbackX = 13f;
+    public float knockbackY = 3f;
 
     Transform playerRoot;
+    Collider2D hitboxCol;
+    bool smacked = false;
 
     void Awake()
     {
         playerRoot = transform.root;
+        hitboxCol = GetComponent<Collider2D>();
+    }
+    void OnEnable()
+    {
+        smacked = false;
+        if (hitboxCol != null)
+        {
+            hitboxCol.enabled = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (smacked)
+        {
+            return;
+        }
         // enemies only
         if (other.CompareTag("Player")) return;
 
@@ -24,6 +39,8 @@ public class PlayerAttackHitbox : MonoBehaviour
         if (hp == null) return;
 
         hp.TakeDamage(damage);
+
+        smacked = true;
 
         Rigidbody2D enemyRb = other.attachedRigidbody;
         if (enemyRb != null)
@@ -34,6 +51,11 @@ public class PlayerAttackHitbox : MonoBehaviour
 
             enemyRb.linearVelocity = Vector2.zero; // reset current motion
             enemyRb.AddForce(new Vector2(dir * knockbackX, knockbackY), ForceMode2D.Impulse);
+        }
+
+        if (hitboxCol != null)
+        {
+            hitboxCol.enabled = false; // itll come back on nextattack
         }
     }
 }
