923d84a25226f174bdde3e31772ac9c18c2cf135
shiruminomii <phuongdangjapan123@gmail.com>
2026-02-09 21:08:09 -0800

    add main menu and pause menu

--- Assets/Scripts/TitleFadeSequence.cs
+++ Assets/Scripts/TitleFadeSequence.cs
@@ -43,7 +43,7 @@ public class TitleFadeSequence : MonoBehaviour
 
         if (autoLoadNextScene)
         {
-            SceneManager.LoadScene(nextSceneName);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
