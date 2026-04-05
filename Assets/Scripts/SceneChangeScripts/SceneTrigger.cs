11dd76da72e5d5303025ac6dd575ecbd21dee9a2
renoutoften <nguyenren64@gmail.com>
2026-04-05 04:40:50 -0700

    scene transition/fade in added

--- Assets/Scripts/SceneChangeScripts/SceneTrigger.cs
+++ Assets/Scripts/SceneChangeScripts/SceneTrigger.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
+using System.Collections;
+using TMPro;
 
 public class SceneTrigger : MonoBehaviour
-{
+{    [SerializeField] private SceneFader sceneFader;
+    [SerializeField] private string transitionText = "MINDSCAPE: The Crows' Laboratory";
+
+    private bool triggered = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !triggered)
         {
-            SceneControl.instance.NextLevel();
+            triggered = true;
+            StartCoroutine(Transition());
         }
+    }
+
+    private IEnumerator Transition()
+    {
+        // fade to blakc
+        sceneFader.FadeToBlack(1.5f);
+
+        yield return new WaitForSeconds(1.5f);
+
+        // fade rexr
+        yield return sceneFader.StartCoroutine(
+            sceneFader.FadeTextRoutine(transitionText, 1f, 2f, 1f)
+        );
+        SceneControl.instance.NextLevel();
     }
 }
