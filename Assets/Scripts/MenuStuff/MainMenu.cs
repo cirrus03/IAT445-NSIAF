7a50afca89264509c53e2052e7f226d6c8096114
renoutoften <nguyenren64@gmail.com>
2026-04-06 17:04:50 -0700

    scene trasnitions

--- Assets/Scripts/MenuStuff/MainMenu.cs
+++ Assets/Scripts/MenuStuff/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     [Header("Scene")]
     [SerializeField] private SceneFader sceneFader;
-    [SerializeField] private string firstGameScene = "NewSidescroll";
+    [SerializeField] private string firstGameScene = "SS_1";
 
     [Header("Panels")]
     [SerializeField] private GameObject mainMenuPanel;
